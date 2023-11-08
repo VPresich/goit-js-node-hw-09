3c1a6944e99356b23ac8bd1c778acaa30414623f
VPresich <111353667+VPresich@users.noreply.github.com>
2023-11-08 21:27:12 +0000

    Deploying to gh-pages from @ VPresich/goit-js-node-hw-09@9f27b5408c7470ce97ffdc01ab81eb580b97eebb 🚀

--- assets/task-9-2-5beb52cf.js
+++
@@ -1,1 +0,0 @@
-import"./styles-7e4ae9d7.js";import{c,i as o}from"./gallery-images-8ad391c9.js";const a=document.querySelector(".gallery-lightbox");c(o,a);a.addEventListener("click",i);function i(e){const t=e.target;if(!t.classList.contains("gallery-img"))return;e.preventDefault();const r={src:t.dataset.source,alt:t.alt,preview:t.src};l(r)}function l(e){console.table(e)}
