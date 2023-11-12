c9d18cbc64588072e834cb5785e6871a88663a9c
VPresich <111353667+VPresich@users.noreply.github.com>
2023-11-12 04:18:53 +0000

    Deploying to gh-pages from @ VPresich/goit-js-node-hw-09@7b42428fb661fafde4dbf130303ffe65b0b2b91c 🚀

---
+++ assets/task-9-2-fcd7c707.js
@@ -0,0 +1,3 @@
+import"./styles-ec282632.js";import{b as s}from"./basicLightbox.min-29bcc645.js";import{c,i as r,K as l}from"./gallery-images-e4621d11.js";let t={};const i=document.querySelector(".gallery");c(r,i);i.addEventListener("click",m);function m(e){const o=e.target;if(!o.classList.contains("gallery-img"))return;e.preventDefault();const n={src:o.dataset.source,alt:o.alt,preview:o.src};d(n)}function d({src:e,alt:o}){t=s.create(`<div class="lightbox-modal ">   
+        <img class="lightbox-image" src="${e}" alt="${o}"/>           
+    </div>`,{onShow:()=>{document.addEventListener("keydown",a)},onClose:()=>{document.removeEventListener("keydown",a)},closable:!0}),t.show()}function a(e){e.code===l&&t.close()}
